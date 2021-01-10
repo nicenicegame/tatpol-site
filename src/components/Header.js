7f4f3e663314a3c266f7b2055f4d06a6a5295174
Nice <nicenicegame@gmail.com>
2021-01-11 01:50:06 +0700

    Add hamburger animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,38 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const Header = () => {
-    const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(false)
 
-    const openSideNav = () => {
-        setToggle(!toggle);
-    };
+  const openSideNav = (e) => {
+    setToggle(!toggle)
+    e.target.classList.toggle('active')
+  }
 
-    return (
-        <header>
-            <a href="#" className="logo">
-                Tatpol S.
-            </a>
-            <nav>
-                <ul className={toggle ? "open-nav" : ""}>
-                    <li>
-                        <a href="#about">About</a>
-                    </li>
-                    <li>
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contact</a>
-                    </li>
-                </ul>
-            </nav>
-            <div className="hamburger" onClick={openSideNav}>
-                <div className="line line1"></div>
-                <div className="line line2"></div>
-                <div className="line line3"></div>
-            </div>
-        </header>
-    );
-};
+  return (
+    <header>
+      <a href="#" className="logo">
+        Tatpol S.
+      </a>
+      <nav>
+        <ul className={toggle ? 'open-nav' : ''}>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="hamburger" onClick={openSideNav}>
+        <div className="line line1"></div>
+        <div className="line line2"></div>
+        <div className="line line3"></div>
+      </div>
+    </header>
+  )
+}
 
-export default Header;
+export default Header
