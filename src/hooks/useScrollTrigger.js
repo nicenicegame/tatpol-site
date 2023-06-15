6f33f3ff666c896a8b1b4569e143e12bec607cf7
nicenicegame <nicenicegame@gmail.com>
2023-06-15 12:34:36 +0700

    fix throttle animation on scroll

--- src/hooks/useScrollTrigger.js
+++ src/hooks/useScrollTrigger.js
@@ -1,14 +1,17 @@
 import { useAnimation, useViewportScroll } from 'framer-motion'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 export const useScrollTrigger = (scroll) => {
+  const isTriggered = useRef(false)
   const controls = useAnimation()
   const { scrollY } = useViewportScroll()
 
   useEffect(() => {
     controls.start('show')
     const unsubscribeOnScrollYChange = scrollY.onChange((value) => {
+      if (isTriggered.current) return
       if (value > scroll) {
+        isTriggered.current = true
         controls.start('exit')
       }
     })
